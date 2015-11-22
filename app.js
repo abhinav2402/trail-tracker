0aca0541df0841d4eea911d02694f4ed8c52a103
Abhinav <nishuabhi@gmail.com>
2015-11-22 12:17:43 -0800

    using bcryptjs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var router = express.Router(); // get an instance of the express Router
-var morgan = require('morgan');
+//var morgan = require('morgan');
 
 var Post = require('./app/models/post')
 var bodyParser = require('body-parser');
